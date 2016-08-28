69b27f56087d6fc0487bf7a0a1fe1956f9e0da0f
Charlie Huguenard <charlie.huguenard@gmail.com>
2016-08-28 12:50:36 -0700

    added comments for lesson 1

--- Assets/Lesson1_Sampler/Scripts/SamplerVoice_Lesson1.cs
+++ Assets/Lesson1_Sampler/Scripts/SamplerVoice_Lesson1.cs
@@ -4,14 +4,25 @@ using System.Collections;
 [RequireComponent(typeof(AudioSource))]
 public class SamplerVoice_Lesson1 : MonoBehaviour
 {
+    // The attached AudioSource component
+    // Note: the RequireComponent attribute above will automatically attach
+    // an AudioSource component to any GameObject that we attach this script to
     private AudioSource _audioSource;
 
+    /// <summary>
+    /// Plays the provided AudioClip on this voice's AudioSource
+    /// </summary>
+    /// <param name="audioClip">The AudioClip we want to play</param>
     public void Play(AudioClip audioClip)
     {
         _audioSource.clip = audioClip;
         _audioSource.Play();
     }
 
+    /// <summary>
+    /// This gets called when the GameObject is first created.
+    /// Do our initialization here, which is just grabbing the AudioSource component.
+    /// </summary>
     private void Awake()
     {
         _audioSource = GetComponent<AudioSource>();
--- Assets/Lesson1_Sampler/Scripts/Sampler_Lesson1.cs
+++ Assets/Lesson1_Sampler/Scripts/Sampler_Lesson1.cs
@@ -4,16 +4,26 @@ using System.Collections;
 public class Sampler_Lesson1 : MonoBehaviour
 {
     [Header("Debug")]
+    // Plays the sound when the toggle is pressed in the editor while the scene is running
     [SerializeField] private bool _debugPlay;
 
     [Header("Config")]
+    // The audio file we want to play
     [SerializeField] private AudioClip _audioClip;
+    // The number of voices. Set this higher if you're hearing excessive voice stealing.
     [SerializeField, Range(1, 8)] private int _numVoices = 2;
+    // The prefab for the sampler voice, which is just a SamplerVoice component attached to a GameObject
     [SerializeField] private SamplerVoice_Lesson1 _samplerVoicePrefab;
 
+    // The list of sampler voices we created during initialization
     private SamplerVoice_Lesson1[] _samplerVoices;
+    // The index of the next voice to play
     private int _nextVoiceIndex;
 
+    /// <summary>
+    /// This gets called when the GameObject first gets created.
+    /// Create our sampler voices here.
+    /// </summary>
     private void Awake()
     {
         _samplerVoices = new SamplerVoice_Lesson1[_numVoices];
@@ -25,21 +35,38 @@ public class Sampler_Lesson1 : MonoBehaviour
             samplerVoice.transform.localPosition = Vector3.zero;
             _samplerVoices[i] = samplerVoice;
         }
+
+        _nextVoiceIndex = 0;
     }
 
+    /// <summary>
+    /// This gets called every game frame.
+    /// Do our debug play here.
+    /// </summary>
     private void Update()
     {
+        // if _debugPlay is true, it means we ticked the toggle in the editor
         if (_debugPlay)
         {
+            // play the sound
             Play();
+            // turn off the toggle
             _debugPlay = false;
         }
     }
 
+    /// <summary>
+    /// Pick a voice and play the sound
+    /// </summary>
     private void Play()
     {
+        // Play the sound on the next voice
         _samplerVoices[_nextVoiceIndex].Play(_audioClip);
-
-        _nextVoiceIndex = (_nextVoiceIndex + 1) % _samplerVoices.Length;
+        // Increment the next voice and wrap it around if necessary
+        ++_nextVoiceIndex;
+        if (_nextVoiceIndex >= _samplerVoices.Length)
+        {
+            _nextVoiceIndex = 0;
+        }
     }
 }
